f80b85461cb24d7e3507be6531b6118865f0e7a0
Connor Yates <yatesco@oregonstate.edu>
2018-05-28 13:36:05 -0700

    Digital pin controlfor arduino-arduino communication, in addition to ROS communication with the computer

--- LauncherROS/LauncherROS.ino
+++ LauncherROS/LauncherROS.ino
@@ -27,34 +27,66 @@ int forward = 120; // retracted position of servo
 int rest = 60;
 int readypin = 13; 
 
+#define COMM_P1 11 //D5
+#define COMM_P2 12 // D6
+//#define COMM_P3 12 // D7
+bool FLAG_P1 = false; 
+bool FLAG_P2 = false; 
+//bool FLAG_P3 = true; 
+
 ros::NodeHandle  nh;
 
 std_msgs::String str_msg;
-char hello[256] = "hello world!";
+char hello[25] = "hello world!";
 
 void setspeed_cb(const std_msgs::UInt16& cmd_msg)
 {
-	/* TODO switch off the command recieved, and set the pins 
-	 * appropriate for that command signal, so the message is
-	 * propogated to the motor controller
-	 */
-	
-	Serial.print("Int array recieved: ");
-	Serial.println(cmd_msg.data);
+	switch (cmd_msg.data)
+	{
+		case 0: 
+			FLAG_P1 = false;
+			FLAG_P2 = false;
+			//FLAG_P3 = false;
+			break;
+		case 1: 
+			FLAG_P1 = true;
+			FLAG_P2 = false;
+			//FLAG_P3 = false;
+			break;
+		case 2: 
+			FLAG_P1 = false;
+			FLAG_P2 = true;
+			//FLAG_P3 = false;
+			break;
+		case 3: 
+			FLAG_P1 = true;
+			FLAG_P2 = true;
+			//FLAG_P3 = false;
+			break;
+		default:
+			FLAG_P1 = false;
+			FLAG_P2 = false;
+			//FLAG_P3 = false;
+			break;
+	}
 }
 
 ros::Subscriber<std_msgs::UInt16> sub("set_speed", setspeed_cb);
 ros::Publisher pub("arduino_debug", &str_msg);
 
-void setup() {
+void setup()
+{
+	pinMode(COMM_P1, OUTPUT);
+	pinMode(COMM_P2, OUTPUT);
+	//pinMode(COMM_P3, OUTPUT);
 	pinMode(readypin, INPUT);
 	myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 	myservo.write(rest);
 
-	Serial.begin(57600);
+	Serial.begin(115200);
 	if (! vcnl.begin())
 	{
-		Serial.println("Sensor not found :(");
+		//Serial.println("Sensor not found :(");
 		while (1);
 	}
 
@@ -63,21 +95,56 @@ void setup() {
 	nh.subscribe(sub);
 }
 
-
-
+void set_comm_pins()
+{
+	if (FLAG_P1)
+	{
+		digitalWrite(COMM_P1, HIGH);
+	}
+	else
+	{
+		digitalWrite(COMM_P1, LOW);
+	}
+	if (FLAG_P2)
+	{
+		digitalWrite(COMM_P2, HIGH);
+	}
+	else
+	{
+		digitalWrite(COMM_P2, LOW);
+	}
+/*
+	if (FLAG_P3)
+	{
+		digitalWrite(COMM_P3, HIGH);
+	}
+	else
+	{
+		digitalWrite(COMM_P3, LOW);
+	}
+*/
+}
 
 void loop() {
 	str_msg.data = hello;
 	pub.publish( &str_msg );
 	nh.spinOnce();
-	Serial.print("Proximity Sensor: ");
-	Serial.println(vcnl.readProximity());
-	if ((vcnl.readProximity() > 4000) && (digitalRead(readypin)==LOW))
+	//Serial.print("Proximity Sensor: ");
+	//Serial.println(vcnl.readProximity());
+	/* Send flywheel command signal */
+	set_comm_pins();
+	nh.spinOnce();
+	delay(100);
+	if ((vcnl.readProximity() > 4000)
+		&& (digitalRead(readypin) == LOW))
 	{
+		nh.spinOnce();
 		myservo.write(back);
 		delay(750);
+		nh.spinOnce();
 		myservo.write(forward);
 		delay(1000);
+		nh.spinOnce();
 		myservo.write(rest);
 		//delay(2500);
 	}
